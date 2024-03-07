90532f4e2b83496de4dd91d719934887a3f3cefb
MrSurajPawar <surajspawar007@gmail.com>
2024-03-07 08:54:41 +0530

    New learnings

--- Express-JS/app.js
+++ Express-JS/app.js
@@ -1,10 +1,27 @@
-const http = require("http"); //importing the core module
+//const http = require("http"); //importing the core module
 
-const app = require('express');  //importing express js here
+const express = require('express');  //importing express js here passed e() function as object
 
-const server = http.createServer( app );
+const app = express();    //Creating an Express Application
 
-const portNo = 4000
-server.listen(portNo, () => {
-  console.log("server is starting...");
+//All Middlewares goes down here
+//Middleware 1
+app.use( (req ,res, next) => {
+  console.log("In the MiddleWare");
+  next();   //allowing the request to proceed to the next middleware in the stack
 });
+
+//Middleware 2
+app.use( (req, res, next) => {
+  console.log("In another Middleware");
+  res.send("<h1>Hello from middleware</h1>");
+});
+
+
+// const server = http.createServer( app );
+
+// const portNo = 4000;
+// server.listen(portNo, () => {
+//   console.log("server is starting...");
+// });
+app.listen(4000);
