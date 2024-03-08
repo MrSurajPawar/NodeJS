f6215def28d6b8ddf043a5a04ae28fc42606eb77
MrSurajPawar <surajspawar007@gmail.com>
2024-03-08 08:01:53 +0530

    Add new chnages

--- Express-JS/app.js
+++ Express-JS/app.js
@@ -1,21 +1,22 @@
 const express = require("express");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const app = express();
 
 //Middlewares
-app.use(bodyParser.urlencoded({extended : false})); 
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get( "/add-product" , (req,res,next) => {
-  res.send("<form action='/product' method='POST'><input type='text' name='title' /><button type='submit'>Submit</button></form>");
+app.get("/add-product", (req, res, next) => {
+  res.send(
+    "<form action='/product' method='POST'><input type='text' name='title' /><button type='submit'>Submit</button></form>"
+  );
 });
 
-app.post("/product" , (req, res, next) => {
-  console.log(req.body);    //to parse the body of the incoming request using the body parser
-  res.redirect("/");
+app.post("/product", (req, res, next) => {
+  console.log(req.body); //to parse the body of the incoming request using the body parser
+  res.redirect("/product");
 });
 
-
-app.get( "/" , (req,res,next) => {
+app.get("/", (req, res, next) => {
   res.send("<h1>Hello From express</h1>");
 });
 
