59217bc223a4e5d886bb2adaf15b09e49c245d99
MrSurajPawar <surajspawar007@gmail.com>
2024-03-08 11:54:09 +0530

    Express LEarning

--- Express-JS-2/app.js
+++ Express-JS-2/app.js
@@ -11,6 +11,8 @@ const bodyParser = require("body-parser");
 
 //Middlewares
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static( path.join(__dirname, "public") ));
+
 app.use("/admin" , adminRoutes);       // Adding filters
 app.use(shopRoutes);
 
